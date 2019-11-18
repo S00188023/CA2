974412ab3824c1188809e62c67b002efc9f73b15
S00188023 <S00188023@users.noreply.github.com>
2019-11-18 12:56:20 +0000

    start

---
+++ CA2/CA2/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    public enum ActivityType { Air, Water, Land}
+    class Activity : IComparable
+    {
+        public string name { get; set; }
+        public DateTime activityDate { get; set; }
+        public decimal cost { get; set; }
+        public Activity typeOfActivity { get; set; }
+        public string description { get; set; }
+
+    }
+
+
+    public Activity CompareTo (DateTime date)
+    {
+        this.CompareTo
+    }
+
+}
